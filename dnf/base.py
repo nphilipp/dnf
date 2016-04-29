ab986180f9c67d37b6fe826c0348cebaefe6d81a
Igor Gnatenko <ignatenko@redhat.com>
2016-04-29 14:53:07 +0200

    Revert "using ts.addReinstall for package reinstallation (RhBug:1071854)"
    This reverts commit 516aad977e108df0f99c0bfc03a25b180888937f.
    
    reinstall stuff implemented in rpm 4.12 which we don't have backported into epel7
    
    Signed-off-by: Igor Gnatenko <ignatenko@redhat.com>

--- dnf/base.py
+++ dnf/base.py
@@ -1603,6 +1603,7 @@ class Base(object):
 
     def reinstall(self, pkg_spec, old_reponame=None, new_reponame=None,
                   new_reponame_neq=None, remove_na=False):
+        self._add_reinstall_rpm_probfilters()
         subj = dnf.subject.Subject(pkg_spec)
         q = subj.get_best_query(self.sack)
         installed_pkgs = [
@@ -1768,6 +1769,7 @@ class Base(object):
                     assert len(obsoleteds_) == 1
                     obsoleteds.append(obsoleteds_[0])
             assert len(news) == 1
+            self._add_reinstall_rpm_probfilters()
             self._transaction.add_reinstall(first(olds), news[0],
                                             obsoleteds)
 
@@ -1914,6 +1916,8 @@ class Base(object):
             myrepos += '\n'
         self.history.write_addon_data('config-repos', myrepos)
 
+    def _add_reinstall_rpm_probfilters(self):
+        self.rpm_probfilter.add(rpm.RPMPROB_FILTER_REPLACEPKG)
 
 def _msg_installed(pkg):
     name = ucd(pkg)
